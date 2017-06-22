3ce5ea333be90256191794853bacba4cc5f89323
Shai Amsalem <shaia@soluto.com>
2017-06-22 13:24:15 +0300

    phpunit-tc is not finding autuload.php so i changed the path to be more accurate

--- phpunit-runner-teamcity/phpunit-tc.php
+++ phpunit-runner-teamcity/phpunit-tc.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'PHPUnit/Autoload.php';
+require_once '../data/vendor/Autoload.php';
 
 class TeamCity_PHPUnit_Framework_TestListener
     implements PHPUnit_Framework_TestListener
