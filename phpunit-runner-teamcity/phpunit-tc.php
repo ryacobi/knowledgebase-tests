50954b11147a7651011af209ad25546d33990864
Shai Amsalem <shaia@soluto.com>
2017-06-22 19:18:07 +0300

    Changed the path to phpunit

--- phpunit-runner-teamcity/phpunit-tc.php
+++ phpunit-runner-teamcity/phpunit-tc.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('./usr/share/php/PHPUnit/Autoload.php');
+require_once ('/usr/share/php/PHPUnit/Autoload.php');
 
 class TeamCity_PHPUnit_Framework_TestListener
     implements PHPUnit_Framework_TestListener
